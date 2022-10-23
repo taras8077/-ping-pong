14ababf0fad7f2acb1ddc4f84f90a20e3eb86b8f
taras8077 <115397197+taras8077@users.noreply.github.com>
2022-10-23 12:02:22 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,11 @@
+from pygame import *
+
+клас-батько
+class GameSprite(sprite.Sprite): def init(self): pass def reset(self): pass
+
+клас для ракеток
+class Player(GameSprite): def update_right(self): pass def update_left(self): pass
+
+win_width = 600 # window width win_height = 500 # window height
+
+window = display.set_mode((win_width, win_height)) fon = (200, 255, 255) window.fill(fon) game=True finish=False clock=time.Clock() FPS=60 While game: for e in event.get: if e == QUIT: game=False if finish != True: window.fill(fon) clock.tick(FPS)
