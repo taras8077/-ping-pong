75c84fbd3a63f1a609893d70a583a123c851b76c
taras8077 <115397197+taras8077@users.noreply.github.com>
2022-10-23 12:26:20 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,48 @@
 from pygame import *
-
-клас-батько
-class GameSprite(sprite.Sprite): def init(self): pass def reset(self): pass
+from pygame import*
+#клас-батько
+class GameSprite(sprite.Sprite): 
+    def __init__(self,player_image,p_x,p_y,p_speed,width,height): 
+        super().__init__()
+        self.image=transform.scale(image.load(player_image),(width,height))
+        self.speed=p_speed
+        self.rect=self.image.get_rect()
+        self.rect.x=p_x
+        self.rect.y=p_y
+    def reset(self): 
+        window.blit(self.image,(self.rect.x, self.rect.y))
 
 клас для ракеток
-class Player(GameSprite): def update_right(self): pass def update_left(self): pass
+class Player(GameSprite): 
+    def update_right(self): 
+        keys=key.get_pressed()
+        if[K_UP] and self.rect.y>5:
+            self.rect.y-=self.speed
+        if[K_DOWN] and self.rect.y<420:
+            self.rect.y+=self.speed
+    def update_left(self): 
+        keys=key.get_pressed()
+        if[K_w] and self.rect.y>5:
+            self.rect.y-=self.speed
+        if[K_s] and self.rect.y<420:
+            self.rect.y+=self.speed
+P1=Player("",)
+win_width = 600
+# window width 
+win_height = 500
+# window height
 
-win_width = 600 # window width win_height = 500 # window height
-
-window = display.set_mode((win_width, win_height)) fon = (200, 255, 255) window.fill(fon) game=True finish=False clock=time.Clock() FPS=60 While game: for e in event.get: if e == QUIT: game=False if finish != True: window.fill(fon) clock.tick(FPS)
+window = display.set_mode((win_width, win_height)) 
+fon = (200, 255, 255) 
+window.fill(fon) 
+game=True 
+finish=False 
+clock=time.Clock() 
+FPS=60 
+While game: 
+    for e in event.get: 
+        if e == QUIT: 
+            game=False 
+    if finish != True: 
+        window.fill(fon) 
+        clock.tick(FPS)
